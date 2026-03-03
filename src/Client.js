9a42c0cc5c53f612c30b5e992e0d7368e249f3ce
Stefan Fuchs <stefan1234@gmail.com>
2026-03-03 19:05:46 -0300

    fix: getProfilePicUrl

--- src/Client.js
+++ src/Client.js
@@ -1578,10 +1578,8 @@ class Client extends EventEmitter {
     async getProfilePicUrl(contactId) {
         const profilePic = await this.pupPage.evaluate(async contactId => {
             try {
-                const chatWid = window.Store.WidFactory.createWid(contactId);
-                return window.compareWwebVersions(window.Debug.VERSION, '<', '2.3000.0')
-                    ? await window.Store.ProfilePic.profilePicFind(chatWid)
-                    : await window.Store.ProfilePic.requestProfilePicFromServer(chatWid);
+                const chat = await window.WWebJS.getChat(contactId);
+                return await window.require('WAWebContactProfilePicThumbBridge').requestProfilePicFromServer(chat); 
             } catch (err) {
                 if(err.name === 'ServerStatusCodeError') return undefined;
                 throw err;
